dd312e6236425d4f011afa4edd81e825facd09b1
Allyandaru <all4yandaru@gmail.com>
2023-11-16 16:18:00 +0700

    add route

---
+++ app/routes/orderitems.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const allowedTo = require("../constants/permissions");
+const { tokenAuth } = require("../middlewares/tokenAuth");
+const { permissionCheck } = require("../middlewares/permissionAuth");
+
+router.use(tokenAuth);
+
+router.get("/", (req, res) => {
+  res.json("orderitems");
+});
+
+module.exports = router;
