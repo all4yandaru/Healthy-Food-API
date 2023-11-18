375672eb3244ae8bb865e60faeb9feddafdfdeb0
Allyandaru <all4yandaru@gmail.com>
2023-11-18 13:27:01 +0700

    User API

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,16 +1,106 @@
 const express = require("express");
-const { user } = require("../models");
+const { user, token } = require("../models");
 const router = express.Router();
 const allowedTo = require("../constants/permissions");
 const { tokenAuth } = require("../middlewares/tokenAuth");
 const { permissionCheck } = require("../middlewares/permissionAuth");
+const {
+  UserRegisterValidator,
+  UserUpdateValidator,
+} = require("../supports/validator/UserValidator");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
+// REGISTER ===============================================================================
+router.post(
+  "/register",
+  new UserRegisterValidator().validate(),
+  async (req, res) => {
+    const roleId = req.body.roleId;
+    if (roleId != 1 && roleId != 2) {
+      res.send("role must be 1 or 2");
+    }
+    req.body.createdAt = new Date();
+    req.body.updatedAt = new Date();
+    await user.create(req.body).then(() => {
+      res.status(200).send("user added successfully");
+    });
+  }
+);
+
+// NEED AUTHORIZATION =====================================================================
 router.use(tokenAuth);
 
-router.get("/", permissionCheck(allowedTo.BROWSE_USERS), (req, res) => {
-  user.findAll().then((users) => {
+// GET ALL USERS (ADMIN ONLY) =============================================================
+router.get("/", permissionCheck(allowedTo.BROWSE_USERS), async (req, res) => {
+  await user.findAll({ include: "role" }).then((users) => {
     res.json(users);
   });
 });
+
+// GET OWN USER DATA BY ID ================================================================
+router.get("/:id", async (req, res) => {
+  // get token data
+  const tokenData = await token.findOne({
+    where: { token: req.headers["authorization"] },
+    include: "user",
+  });
+
+  // check id
+  const idUser = parseInt(req.params.id);
+  if (tokenData.user.id === idUser) {
+    const userData = await user.scope("withPassword").findOne({
+      where: { id: idUser },
+    });
+    res.json(userData);
+  } else {
+    res.send("You are not allowed to access other users data");
+  }
+});
+
+// UPDATE USER BY ID ===================================================================
+router.post(
+  "/update/:id",
+  new UserUpdateValidator().validate(),
+  async (req, res) => {
+    // mendapatkan user data dari token
+    const tokenData = await token.findOne({
+      where: { token: req.headers["authorization"] },
+      include: "user",
+    });
+
+    // cheking user data dengan body
+    const idUser = parseInt(req.params.id);
+    if (tokenData.user.id === idUser) {
+      const userData = await user.scope("withPassword").findOne({
+        where: { id: idUser, email: req.body.email },
+      });
+
+      if (userData !== null) {
+        // Change all data
+        userData.name = req.body.name;
+        userData.email = req.body.newEmail;
+        userData.phone = req.body.phone;
+        userData.updatedAt = new Date();
+        // encrypt password
+        bcrypt
+          .hash(req.body.password, parseInt(process.env.BCRYPT_ROUND) || 10)
+          .then((hash) => {
+            userData.password = hash;
+          })
+          .catch((Err) => {
+            throw new Error();
+          });
+
+        await userData.save();
+        res.send("user updated");
+      } else {
+        res.send("user is not found");
+      }
+    } else {
+      res.send("Data is not match");
+    }
+  }
+);
 
 module.exports = router;
---
+++ app/supports/validator/UserValidator.js
@@ -0,0 +1,77 @@
+const { check } = require("express-validator");
+const { user } = require("../../models");
+const Validator = require("../Validator");
+const express = require("express");
+
+class UserRegisterValidator extends Validator {
+  rules = [
+    check("name")
+      .notEmpty()
+      .isLength({ min: 1, max: 255 })
+      .withMessage("Name must be between 1 and 255 characters"),
+
+    check("email")
+      .notEmpty()
+      .isEmail()
+      .isLength({ min: 1, max: 255 })
+      .custom(async (email) => {
+        const checkEmail = await user.findOne({ where: { email: email } });
+        if (checkEmail !== null) {
+          throw Error(`email ${email} is already used`);
+        }
+      }),
+
+    check("password")
+      .notEmpty()
+      .isLength({ min: 1, max: 255 })
+      .withMessage("Password must be between 1 and 255 characters"),
+
+    check("phone")
+      .notEmpty()
+      .isLength({ min: 12, max: 13 })
+      .withMessage("phone must be between 12 and 13 numbers"),
+
+    check("roleId")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("role id must be between 1 and 2 numbers"),
+  ];
+}
+
+class UserUpdateValidator extends Validator {
+  rules = [
+    check("name")
+      .notEmpty()
+      .isLength({ min: 1, max: 255 })
+      .withMessage("Name must be between 1 and 255 characters"),
+
+    check("email")
+      .notEmpty()
+      .isEmail()
+      .isLength({ min: 1, max: 255 })
+      .withMessage("Email must be between 1 and 255 characters"),
+
+    check("newEmail")
+      .notEmpty()
+      .isEmail()
+      .isLength({ min: 1, max: 255 })
+      .custom(async (email) => {
+        const checkEmail = await user.findOne({ where: { email: email } });
+        if (checkEmail !== null) {
+          throw Error(`email ${email} is already used`);
+        }
+      }),
+
+    check("password")
+      .notEmpty()
+      .isLength({ min: 1, max: 255 })
+      .withMessage("Password must be between 1 and 255 characters"),
+
+    check("phone")
+      .notEmpty()
+      .isLength({ min: 12, max: 13 })
+      .withMessage("phone must be between 12 and 13 numbers"),
+  ];
+}
+
+module.exports = { UserRegisterValidator, UserUpdateValidator };
