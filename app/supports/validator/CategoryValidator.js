e1d7357e18f7f60d38e63c6483327a680a52e3e3
boodat <bastianhaloho12@gmail.com>
2023-11-18 15:02:58 +0700

    Category API

---
+++ app/supports/validator/CategoryValidator.js
@@ -0,0 +1,29 @@
+const { check } = require("express-validator");
+const { category } = require("../../models");
+const Validator = require("../Validator");
+const express = require("express");
+
+class CategoryAddValidator extends Validator {
+  rules = [
+    check("name")
+      .notEmpty()
+      .isLength({ min: 1, max: 255 })
+      .custom(async (categoryValue) => {
+        const checkCategory = await category.findOne({ where: { name: categoryValue } });
+        if (checkCategory !== null) {
+          throw Error(`Category ${categoryValue} is already used`);
+        }
+      }),
+  ];
+}
+
+class CategoryUpdateValidator extends Validator {
+    rules = [
+      check("name")
+        .notEmpty()
+        .isLength({ min: 1, max: 255 })
+        .withMessage("Name must be between 1 and 255 characters")
+    ]
+}
+
+module.exports = {CategoryAddValidator, CategoryUpdateValidator}
