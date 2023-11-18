90d077715eace7c9456a3d68d8082b7469d7714f
Allyandaru <all4yandaru@gmail.com>
2023-11-19 00:51:29 +0700

    Add Cart Item

--- app/routes/cartitems.js
+++ app/routes/cartitems.js
@@ -1,62 +1,112 @@
 const express = require("express");
-const { cartitem, token } = require("../models");
+const { cartitem, token, product } = require("../models");
 const router = express.Router();
 const allowedTo = require("../constants/permissions");
 const { tokenAuth } = require("../middlewares/tokenAuth");
 const { permissionCheck } = require("../middlewares/permissionAuth");
+const { CartAddValidator } = require("../supports/validator/CartItemValidator");
 
 router.use(tokenAuth);
 
-router.get("/getAll", 
-permissionCheck(allowedTo.BROWSE_CARTITEMS),
-async (req, res) => {
-  const cartData = await cartitem.findAll();
-  res.json(cartData)
-});
+// Admin =================================================================================
+// get All Cart Data (Admin only)
+router.get(
+  "/getAll",
+  permissionCheck(allowedTo.BROWSE_CARTITEMS),
+  async (req, res) => {
+    const cartData = await cartitem.findAll();
+    res.json(cartData);
+  }
+);
 
-router.get("/", 
-permissionCheck(allowedTo.READ_CARTITEM),
-async (req, res) => {
+// Customer ==============================================================================
+// Get Cart item
+router.get("/", permissionCheck(allowedTo.READ_CARTITEM), async (req, res) => {
   const tokenData = await token.findOne({
-    where: { token: req.headers["authorization"]},
+    where: { token: req.headers["authorization"] },
     include: "user",
   }); // ambil id user dari token
-  
-  cartitem.findAll({ where: {userId: tokenData.user.id }}).then((cartitem) => {
-    res.json(cartitem);
-  });
+
+  cartitem
+    .findAll({ where: { userId: tokenData.user.id } })
+    .then((cartitem) => {
+      res.json(cartitem);
+    });
 });
 
+// Add Cart
+router.post(
+  "/",
+  permissionCheck(allowedTo.READ_CARTITEM),
+  new CartAddValidator().validate(),
+  async (req, res) => {
+    // ambil data user
+    const tokenData = await token.findOne({
+      where: { token: req.headers["authorization"] },
+      include: "user",
+    });
+
+    // ambil data produk
+    const productData = await product.findOne({
+      where: { id: req.body.productId },
+    });
+    if (productData != null) {
+      // cek quantity & stock
+      if (productData.stock > req.body.quantity) {
+        /* 
+        update stock product
+        const stock = productData.stock - req.body.quantity;
+        console.log("stock sisa: ", stock);
+        productData.stock = stock;
+        await productData.save(); 
+        */
+
+        // add data
+        await cartitem.create({
+          userId: tokenData.user.id,
+          quantity: req.body.quantity,
+          note: req.body.note ?? null,
+          productId: productData.id,
+        });
+        res.send("cart added");
+      } else {
+        res.send("stock kurang");
+      }
+    } else {
+      res.send("product not found");
+    }
+  }
+);
+
+// Delete Cart
 router.delete(
   "/:id",
   permissionCheck(allowedTo.DELETE_CARTITEM),
-  async (req, res) => { 
-    const cartData = await cartitem.findOne ({
-      where: { id: req.params.id }, 
+  async (req, res) => {
+    const cartData = await cartitem.findOne({
+      where: { id: req.params.id },
       include: "user",
     }); // ambil id user dari parameter id dari cart item
-    
-    cartData==null?res.send("no cart item"):0;
+
+    cartData == null ? res.send("no cart item") : 0;
 
     const tokenData = await token.findOne({
-      where: { token: req.headers["authorization"]},
+      where: { token: req.headers["authorization"] },
       include: "user",
-    }); // ambil user id dari token 
-    
-    if (cartData.user.id == tokenData.user.id){
+    }); // ambil user id dari token
 
-    // di check apakah sama idnya? jika sama maka delete success
-    // jika idnya beda maka delete failed
-      await cartitem
-        .destroy({
-          where: {
-            id: req.params.id,
-          },
-        }) 
-        res.send("Delete Success");
+    if (cartData.user.id == tokenData.user.id) {
+      // di check apakah sama idnya? jika sama maka delete success
+      // jika idnya beda maka delete failed
+      await cartitem.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send("Delete Success");
     } else {
       res.send("id tidak sama");
-    } 
+    }
   }
 );
 
---
+++ app/supports/validator/CartItemValidator.js
@@ -0,0 +1,20 @@
+const { check } = require("express-validator");
+const { cartitem } = require("../../models");
+const Validator = require("../Validator");
+const express = require("express");
+
+class CartAddValidator extends Validator {
+  rules = [
+    check("quantity")
+      .notEmpty()
+      .isInt({ min: 1 })
+      .withMessage("quantity must be minimum 1"),
+
+    check("productId")
+      .notEmpty()
+      .isInt()
+      .withMessage("productId must be minimum 1"),
+  ];
+}
+
+module.exports = { CartAddValidator };
