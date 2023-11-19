d9de316a823f2e339afc3a861a476b7c021526ed
Allyandaru <all4yandaru@gmail.com>
2023-11-19 10:31:04 +0700

    Update Product API

--- app/routes/products.js
+++ app/routes/products.js
@@ -6,7 +6,6 @@ const { tokenAuth } = require("../middlewares/tokenAuth");
 const { permissionCheck } = require("../middlewares/permissionAuth");
 const {
   ProductAddValidator,
-  ProductUpdateValidator,
 } = require("../supports/validator/ProductValidator");
 
 router.use(tokenAuth);
@@ -43,18 +42,30 @@ router.post(
 router.put(
   "/:id",
   permissionCheck(allowedTo.EDIT_PRODUCT),
-  new ProductUpdateValidator().validate(),
   async (req, res) => {
     const productData = await product.findOne({ where: { id: req.params.id } });
 
     if (productData != null) {
       // update here
-      if (productData.name != req.body.name) {
-        const checkProduct = await product.findOne({
-          where: { name: req.body.name },
-        });
-        if (checkProduct != null) {
-          res.send("product name has been already used");
+      if (req.body.name) {
+        // cek ada nama atau engga
+        if (productData.name != req.body.name) {
+          const checkProduct = await product.findOne({
+            where: { name: req.body.name },
+          });
+          if (checkProduct != null) {
+            res.send("product name has been already used");
+          } else {
+            productData.name = req.body.name;
+            productData.calories = req.body.calories;
+            productData.description = req.body.description;
+            productData.price = req.body.price;
+            productData.rating = req.body.rating;
+            productData.stock = req.body.stock;
+            productData.image = req.body.image;
+            productData.save();
+            res.send("Data Updated");
+          }
         } else {
           productData.name = req.body.name;
           productData.calories = req.body.calories;
@@ -67,7 +78,6 @@ router.put(
           res.send("Data Updated");
         }
       } else {
-        productData.name = req.body.name;
         productData.calories = req.body.calories;
         productData.description = req.body.description;
         productData.price = req.body.price;
--- app/supports/validator/ProductValidator.js
+++ app/supports/validator/ProductValidator.js
@@ -42,38 +42,4 @@ class ProductAddValidator extends Validator {
   ];
 }
 
-class ProductUpdateValidator extends Validator {
-  rules = [
-    check("name")
-      .notEmpty()
-      .isLength({ min: 1, max: 255 })
-      .withMessage("name must be between 1 and 255 characters"),
-
-    check("calories")
-      .notEmpty()
-      .isInt({ min: 1 })
-      .withMessage("calories must be minimum 1"),
-
-    check("description")
-      .notEmpty()
-      .isLength({ min: 1 })
-      .withMessage("description must be minimum 1 characters"),
-
-    check("price")
-      .notEmpty()
-      .isInt({ min: 500 })
-      .withMessage("calories must be minimum Rp 500"),
-
-    check("rating")
-      .notEmpty()
-      .isInt({ min: 1, max: 100 })
-      .withMessage("calories must be between 1 and 100"),
-
-    check("stock")
-      .notEmpty()
-      .isInt({ min: 1 })
-      .withMessage("calories must be minimum 1"),
-  ];
-}
-
-module.exports = { ProductAddValidator, ProductUpdateValidator };
+module.exports = { ProductAddValidator };
